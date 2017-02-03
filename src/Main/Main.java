e2d18c423b1b792b05b7e85b2670ccc0eb67b276
Joe <Joe@localhost>
2017-02-03 12:18:28 -0500

    I actualy uploaded it this time

---
+++ src/Main/Main.java
@@ -0,0 +1,13 @@
+package Main;
+
+/**
+ *  Just made this stuff to make sure Eclipse and GitHub are working correctly!!!!
+ */
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello");
+	}
+}
