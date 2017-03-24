35cfc9891f8148af0c80c55c30b5a8be0febccdd
Jacklyn J Videira <jacklyn.videira@uconn.edu>
2017-03-24 13:41:57 -0400

    Update Player.java

--- src/model/Player.java
+++ src/model/Player.java
@@ -2,8 +2,39 @@ package model;
 
 public class Player
 {
-	private FinancialStatement _fs;
+	public FinacialStatement _fs; //financial statement of user
+	//idNumber?
+//	public boolean _charity;
+	private int _CharityCount; //charity counter
+	private int _DownsizeCount; //downsize counter
+	private int _location;
+	private boolean _winner = false;
 	
+	public boolean hasWon() {
+		return _winner;
+	}
+	
+	//needs to roll dice
+	public void pickCard() {
+		
+	}
+	
+	public boolean donateCharity() { 
+		//
+		return false;
+	}
+	
+	public void setLocation() {
+		
+	}
+	
+	public int getLocation() {
+		return this._location;
+	}
+	
+	public Player() { //constructer 
+		
+	}
 	
 	
 }
