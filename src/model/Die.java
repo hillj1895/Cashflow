bfddbf5c55039f402d0b9d521c41a7a364023a85
Jacklyn J Videira <jacklyn.videira@uconn.edu>
2017-03-24 13:40:53 -0400

    Update Die.java

--- src/model/Die.java
+++ src/model/Die.java
@@ -6,20 +6,21 @@ public class Die {
  * Die will probably have to go in each individual turn, so that it can pull charity status
  * from the player currently playing
  */
-	public int rollOneDice()
-	{
+	public int rollDice(Boolean c) {
 		Random rand = new Random();
-		int face = 1+rand.nextInt(6);
-		return face;
+		//if statement for downsize first
+			//if statement for charity next
+				//then do the rest
+		if (c) {
+			int face = 1+rand.nextInt(12);
+			return face;
+			/* There needs to be something that takes charity - 1 from the player after rolling
+			 * Or maybe just a simple "if charity > 0 then charity - 1 after every turn
+			 */
+		}
+		else {
+			int face = 1+rand.nextInt(6);
+			return face;
+		}
 	}
-	
-	public int rollTwoDice()
-	{
-		Random rand = new Random();
-		int face = 1+rand.nextInt(12);
-		return face;
-	}
-	
-	
-	
 }
